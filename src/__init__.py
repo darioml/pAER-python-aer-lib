19205e74b89a12ea4d06a1b06efcef8ad511f1b6
Michael Hart <michael.hart1994@gmail.com>
2017-02-14 13:03:37 +0000

    Changed variables, formatting, docstrings to match linter

--- src/__init__.py
+++ src/__init__.py
@@ -5,64 +5,67 @@ Description: main file for python-ae
 """
 
 from __future__ import print_function
-from PIL import Image
+import copy
 import math
-import numpy as np
-import scipy.io
 import os
 import time
+from PIL import Image
+import numpy as np
+import scipy.io
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
 
-class aefile(object):
+class AEFile(object):
+    """Class representing AER data file"""
     def __init__(self, filename, max_events=1e6):
         self.filename = filename
         self.max_events = max_events
         self.header = []
         self.data, self.timestamp = self.read()
 
-    # alias for read
     def load(self):
+        """Alias for read"""
         return self.read()
 
     def read(self):
-        with open(self.filename, 'r') as f:
-            line = f.readline()
+        """Read data from file, ignoring comments"""
+        with open(self.filename, 'r') as aef:
+            line = aef.readline()
             while line[0] == '#':
                 self.header.append(line)
                 if line[0:9] == '#!AER-DAT':
                     aer_version = line[9]
-                current = f.tell()
-                line = f.readline()
+                current = aef.tell()
+                line = aef.readline()
 
             if aer_version != '2':
                 raise Exception('Invalid AER version. '
                                 'Expected 2, got {}'.format(aer_version))
 
-            f.seek(0, 2)
-            numEvents = math.floor((f.tell() - current) / 8)
+            aef.seek(0, 2)
+            num_events = math.floor((aef.tell() - current) / 8)
 
-            if numEvents > self.max_events:
+            if num_events > self.max_events:
                 print('There are {} events, but max_events is set to {}. '
-                      'Will only use {} events.'.format(numEvents,
+                      'Will only use {} events.'.format(num_events,
                                                         self.max_events,
                                                         self.max_events))
-                numEvents = self.max_events
+                num_events = self.max_events
 
-            f.seek(current)
+            aef.seek(current)
             j = 0
-            timestamps = np.zeros(numEvents)
-            data = np.zeros(numEvents)
+            timestamps = np.zeros(num_events)
+            data = np.zeros(num_events)
 
-            # print(numEvents)
-            for i in range(int(numEvents)):
-                f.seek(current+8*i)
-                # data[i] = int(f.read(4).encode('hex'), 16)
-                # timestamps[i] = int(f.read(4).encode('hex'), 16)
+            # print(num_events)
+            for i in range(int(num_events)):
+                aef.seek(current+8*i)
+                # data[i] = int(aef.read(4).encode('hex'), 16)
+                # timestamps[i] = int(aef.read(4).encode('hex'), 16)
 
-                cur_data = int(f.read(4).encode('hex'), 16)
-                cur_timestamp = int(f.read(4).encode('hex'), 16)
+                cur_data = int(aef.read(4).encode('hex'), 16)
+                cur_timestamp = int(aef.read(4).encode('hex'), 16)
                 if j > 0:
                     time_diff = cur_timestamp - timestamps[j-1]
                     if 0 <= time_diff <= 1e8:
@@ -78,85 +81,91 @@ class aefile(object):
             return data, timestamps
 
     def save(self, data=None, filename=None, ext='aedat'):
+        """Saves given data to file name"""
         if filename is None:
             filename = self.filename
         if data is None:
-            data = aedata(self)
+            data = AEData(self)
         if ext is 'aedat':
             # unpack our 'data'
-            ts = data.ts
+            timestamp = data.timestamp
             data = data.pack()
 
-            with open(filename, 'w') as f:
+            with open(filename, 'w') as aef:
                 for item in self.header:
-                    f.write(item)
+                    aef.write(item)
                 # print('\n\n')
-                # f.write('\n\n')  # Was this meant to write to the file?
-                current = f.tell()
+                # aef.write('\n\n')  # Was this meant to write to the file?
+                current = aef.tell()
                 num_items = len(data)
                 for i in range(num_items):
-                    f.seek(current+8*i)
-                    f.write(hex(int(data[i]))[2:].zfill(8).decode('hex'))
-                    f.write(hex(int(ts[i]))[2:].zfill(8).decode('hex'))
+                    aef.seek(current+8*i)
+                    aef.write(hex(int(data[i]))[2:].zfill(8).decode('hex'))
+                    aef.write(hex(int(timestamp[i]))[2:].zfill(8).decode('hex'))
 
     def unpack(self):
-        noData = len(self.data)
-        x = np.zeros(noData)
-        y = np.zeros(noData)
-        t = np.zeros(noData)
+        """Unpack x, y axes and time data from loaded data"""
+        no_data = len(self.data)
+        x_data = np.zeros(no_data)
+        y_data = np.zeros(no_data)
+        t_data = np.zeros(no_data)
 
-        for i in range(noData):
-            d = int(self.data[i])
-            t[i] = d & 0x1
-            x[i] = 128-((d >> 0x1) & 0x7F)
-            y[i] = (d >> 0x8) & 0x7F
-        return x, y, t
+        for idx, data in enumerate(no_data):
+            t_data[idx] = data & 0x1
+            x_data[idx] = 128-((data >> 0x1) & 0x7F)
+            y_data[idx] = (data >> 0x8) & 0x7F
+        return x_data, y_data, t_data
 
 
-class aedata(object):
+class AEData(object):
+    """Class representation of AER data"""
     def __init__(self, ae_file=None):
         self.dimensions = (128, 128)
-        if isinstance(ae_file, aefile):
-            self.x, self.y, self.t = ae_file.unpack()
-            self.ts = ae_file.timestamp
-        elif isinstance(ae_file, aedata):
-            self.x, self.y, self.t = aedata.x, aedata.y, aedata.t
-            self.ts = ae_file.ts
+        if isinstance(ae_file, AEFile):
+            self.x_data, self.y_data, self.t_data = ae_file.unpack()
+            self.timestamp = ae_file.timestamp
+        elif isinstance(ae_file, AEData):
+            self.x_data, self.y_data, self.t_data = (ae_file.x_data,
+                                                     ae_file.y_data,
+                                                     ae_file.t_data)
+            self.timestamp = ae_file.timestamp
         else:
-            self.x, self.y = np.array([]), np.array([])
-            self.t, self.ts = np.array([]), np.array([])
+            self.x_data, self.y_data = np.array([]), np.array([])
+            self.t_data, self.timestamp = np.array([]), np.array([])
 
     def __getitem__(self, item):
-        rtn = aedata()
-        rtn.x = self.x[item]
-        rtn.y = self.y[item]
-        rtn.t = self.t[item]
-        rtn.ts = self.ts[item]
+        rtn = AEData()
+        rtn.x_data = self.x_data[item]
+        rtn.y_data = self.y_data[item]
+        rtn.t_data = self.t_data[item]
+        rtn.timestamp = self.timestamp[item]
         return rtn
 
     def __setitem__(self, key, value):
-        self.x[key] = value.x
-        self.y[key] = value.y
-        self.t[key] = value.t
-        self.ts[key] = value.ts
+        self.x_data[key] = value.x_data
+        self.y_data[key] = value.y_data
+        self.t_data[key] = value.t_data
+        self.timestamp[key] = value.timestamp
 
     def __delitem__(self, key):
-        self.x = np.delete(self.x, key)
-        self.y = np.delete(self.y, key)
-        self.t = np.delete(self.t, key)
-        self.ts = np.delete(self.ts, key)
+        self.x_data = np.delete(self.x_data, key)
+        self.y_data = np.delete(self.y_data, key)
+        self.t_data = np.delete(self.t_data, key)
+        self.timestamp = np.delete(self.timestamp, key) 
 
     def save_to_mat(self, filename):
-        scipy.io.savemat(filename, {'X': self.x, 'Y': self.y,
-                                    't': self.t, 'ts': self.ts})
+        """Save object data to .mat file using scipy"""
+        scipy.io.savemat(filename, {'X': self.x_data, 'Y': self.y_data,
+                                    't': self.timestamp, 'ts': self.timestamp})
 
     def pack(self):
-        noData = len(self.x)
-        packed = np.zeros(noData)
-        for i in range(noData):
-            packed[i] = (int(self.t[i]) & 0x1)
-            packed[i] += (int(128-self.x[i]) & 0x7F) << 0x1
-            packed[i] += (int(self.y[i]) & 0x7F) << 0x8
+        """Packs data into binary format"""
+        no_data = len(self.x_data)
+        packed = np.zeros(no_data)
+        for i in range(no_data):
+            packed[i] = (int(self.t_data[i]) & 0x1)
+            packed[i] += (int(128-self.x_data[i]) & 0x7F) << 0x1
+            packed[i] += (int(self.y_data[i]) & 0x7F) << 0x8
 
         return packed
 
@@ -164,137 +173,167 @@ class aedata(object):
     # performance here can be improved by allowing indexing in the AE data.
     # For now, I expect this not to be done often
     def make_sparse(self, ratio):
-        indexes = np.random.randint(0, len(self.x),
-                                    math.floor(len(self.x) / ratio))
+        """Random selection of loaded data to produce sparse data set
+
+        Keyword arguments:
+        ratio -- the divisor of the length, e.g. 5 gives 1/5 of the data
+        """
+        indexes = np.random.randint(0, len(self.x_data),
+                                    math.floor(len(self.x_data) / ratio))
         indexes.sort()
 
-        rtn = aedata()
-        rtn.x = self.x[indexes]
-        rtn.y = self.y[indexes]
-        rtn.t = self.t[indexes]
-        rtn.ts = self.ts[indexes]
+        rtn = AEData()
+        rtn.x_data = self.x_data[indexes]
+        rtn.y_data = self.y_data[indexes]
+        rtn.t_data = self.t_data[indexes]
+        rtn.timestamp = self.timestamp[indexes]
 
         return rtn
 
     def __repr__(self):
-        return "%i total [x,y,t,ts]: [%s, %s, %s, %s]".format(len(self.x),
-                                                              self.x, self.y,
-                                                              self.t, self.ts)
+        return "{} total [x,y,t,ts]: [{}, {}, {}, {}]".format(len(self.x_data),
+                                                              self.x_data, 
+                                                              self.y_data,
+                                                              self.t_data, 
+                                                              self.timestamp)
 
     def __len__(self):
-        return len(self.x)
+        return len(self.x_data)
 
     def interactive_animation(self, step=5000, limits=(0, 128), pause=0):
+        """Present data in a graph that allows interaction"""
         plt.ion()
         fig = plt.figure(figsize=(6, 6))
         plt.show()
-        ax = fig.add_subplot(111)
+        axes = fig.add_subplot(111)
 
         start = 0
         end = step - 1
-        while(start < len(self.x)):
-            ax.clear()
-            ax.scatter(self.x[start:end], self.y[start:end],
-                       s=20, c=self.t[start:end], marker='o', cmap=cm.jet)
-            ax.set_xlim(limits)
-            ax.set_ylim(limits)
+        while start < len(self.x_data):
+            axes.clear()
+            axes.scatter(self.x_data[start:end], self.y_data[start:end],
+                         s=20, c=self.t_data[start:end], marker='o',
+                         cmap='jet')
+            axes.set_xlim(limits)
+            axes.set_ylim(limits)
             start += step
             end += step
             plt.draw()
             time.sleep(pause)
 
     def downsample(self, new_dimensions=(16, 16)):
+        """Reduces resolution to given dimensions
+
+        Keyword arguments:
+        new_dimensions -- tuple of new dimensions, default (16, 16)
+        """
         # TODO
         # Make this cleaner
         assert self.dimensions[0] % new_dimensions[0] is 0
         assert self.dimensions[1] % new_dimensions[1] is 0
 
-        rtn = aedata()
-
-        rtn.ts = self.ts
-        rtn.t = self.t
-        rtn.x = np.floor(self.x / (self.dimensions[0] / new_dimensions[0]))
-        rtn.y = np.floor(self.y / (self.dimensions[1] / new_dimensions[1]))
+        rtn = AEData()
+        rtn.timestamp = self.timestamp
+        rtn.t_data = self.t_data
+        rtn.x_data = np.floor(self.x_data / 
+                              (self.dimensions[0] / new_dimensions[0]))
+        rtn.y_data = np.floor(self.y_data / 
+                              (self.dimensions[1] / new_dimensions[1]))
 
         return rtn
 
     def to_matrix(self, dim=(128, 128)):
-        return make_matrix(self.x, self.y, self.t, dim=dim)
+        """Returns a matrix of the given dimensions containing AER data
 
-    # Returns new aedata object with given event type removed
-    def filter_events(self, type):
-        if type == 'ON':
-            tp = 0
-        elif type == 'OFF':
-            tp = 1
+        Keyword arguments:
+        dim -- tuple of matrix dimensions, default (128, 128)
+        """
+        return make_matrix(self.x_data, self.y_data, self.t_data, dim=dim)
+
+    def filter_events(self, evt_type):
+        """Returns new AEData object with given event type removed
+
+        Keyword arguments:
+        evt_type -- string of event type, either 'ON' or 'OFF'"""
+        if evt_type == 'ON':
+            allow = 0
+        elif evt_type == 'OFF':
+            allow = 1
         else:
             print('Invalid event type for filter')
             return None
     
-        rtn = aedata()
+        rtn = AEData()
         
-        for i in range(len(self)):
-            if self.t[i] == tp:
-                rtn.ts = np.append(rtn.ts, [self.ts[i]])
-                rtn.t = np.append(rtn.t, [tp])
-                rtn.x = np.append(rtn.x, [self.x[i]])
-                rtn.y = np.append(rtn.y, [self.y[i]])
+        for idx, _type in enumerate(self):
+            if _type == allow:
+                rtn.timestamp = np.append(rtn.timestamp, [self.timestamp[idx]])
+                rtn.t_data = np.append(rtn.t_data, [allow])
+                rtn.x_data = np.append(rtn.x_data, [self.x_data[idx]])
+                rtn.y_data = np.append(rtn.y_data, [self.y_data[idx]])
         return rtn
 
-    # Discards type information by setting type to 0 for every event,
-    # returns result in new aedata object
     def merge_events(self):
+        """Set all events to the same type, packaged in new AEData object"""
         rtn = copy.deepcopy(self)
-        rtn.t = np.zeros(len(self.t))
+        rtn.t_data = np.zeros(len(self.t_data))
         return rtn
-    
-    # Takes n elements from dataset
-    def take(self, n):
-        if (n <= len(self)):
-            return self.make_sparse(len(self)/n)
+
+    def take(self, n_elements):
+        """Returns n_elements from data set by random selection"""
+        if n_elements <= len(self):
+            return self.make_sparse(len(self)/n_elements)
         else:
             print('Number of desired elements more than available')
             return None
 
-    # Takes n elements from dataset
-    # Alternative version, slightly slower, but returned timestamps
-    # are distributed more uniformly
-    def take_v2(self, n):
-        if n > len(self):
+    def take_v2(self, n_elements):
+        """Takes n_elements elements with more even distribution"""
+        if n_elements > len(self):
             print('Number of desired elements more than available')
             return None
-        step = len(self)/n
+        step = len(self)/n_elements
         temp = 0
-        rtn = aedata()
+        rtn = AEData()
         i = 0.0
-        while(i < len(self)):
+        while i < len(self):
             numt = int(np.floor(i))
             if temp != numt:
-                rtn.x = np.append(rtn.x, self.x[numt])
-                rtn.y = np.append(rtn.y, self.y[numt])
-                rtn.t = np.append(rtn.t, self.t[numt])
-                rtn.ts = np.append(rtn.ts, self.ts[numt])
+                rtn.x_data = np.append(rtn.x_data, self.x_data[numt])
+                rtn.y_data = np.append(rtn.y_data, self.y_data[numt])
+                rtn.t_data = np.append(rtn.t_data, self.t_data[numt])
+                rtn.timestamp = np.append(rtn.timestamp, self.timestamp[numt])
                 temp = numt
             i += step
         return rtn
 
-    # Shifts and rescales timestamps, keeps starting point by default
     def change_timescale(self, length, start=None):
+        """Shifts and rescales timestamps, keeps starting point by default"""
         rtn = copy.deepcopy(self)
-        min = np.min(rtn.ts)
+        _min = np.min(rtn.ts)
         if start is None:
-            start = min
-        rtn.ts = np.floor((rtn.ts-min)/((np.max(rtn.ts)-min)/length)+start)
+            start = _min
+        rtn.timestamp = np.floor(
+            (rtn.timestamp - _min) /
+            ((np.max(rtn.timestamp) - _min) / length) + start)
         return rtn
 
 
-def make_matrix(x, y, t, dim=(128, 128)):
+def make_matrix(x_data, y_data, t_data, dim=(128, 128)):
+    """Form matrix from x, y, t data with given dimensions
+
+    Keyword arguments:
+    x_data -- x axis data
+    y_data -- y axis data
+    t_data -- t axis data
+    dim -- tuple of matrix dimensions, default (128, 128)
+    """
     image = np.zeros(dim)
     events = np.zeros(dim)
 
-    for i in range(len(x)):
-        image[y[i]-1, x[i]-1] -= t[i]-0.5
-        events[y[i]-1, x[i]-1] += 1
+    for idx, _time in enumerate(t_data):
+        image[y_data[idx]-1, x_data[idx]-1] -= _time - 0.5
+        events[y_data[idx]-1, x_data[idx]-1] += 1
 
     # http://stackoverflow.com/questions/26248654/numpy-return-0-with-divide-by-zero
     np.seterr(divide='ignore', invalid='ignore')
@@ -305,18 +344,19 @@ def make_matrix(x, y, t, dim=(128, 128)):
 
 
 def create_pngs(data, prepend, path="", step=3000, dim=(128, 128)):
+    """Create PNG files at given location with given dimensions"""
     if not os.path.exists(path):
         os.makedirs(path)
 
     idx = 0
     start = 0
     end = step - 1
-    while(start < len(data.x)):
-        image = make_matrix(data.x[start:end], data.y[start:end],
-                            data.t[start:end], dim=dim)
+    while start < len(data.x_data):
+        image = make_matrix(data.x_data[start:end], data.y_data[start:end],
+                            data.t_data[start:end], dim=dim)
         img_arr = (image*255).astype('uint8')
-        im = Image.fromarray(img_arr)
-        im.save(path + os.path.sep + prepend + ("%05d" % idx) + ".png")
+        im_data = Image.fromarray(img_arr)
+        im_data.save(path + os.path.sep + prepend + ("%05d" % idx) + ".png")
         idx += 1
 
         start += step
@@ -324,12 +364,11 @@ def create_pngs(data, prepend, path="", step=3000, dim=(128, 128)):
 
 
 def concatenate(a_tuple):
-    rtn = aedata()
-    n = len(a_tuple)
-    rtn.x = np.concatenate(tuple([a_tuple[i].x for i in range(n)]))
-    rtn.y = np.concatenate(tuple([a_tuple[i].y for i in range(n)]))
-    rtn.t = np.concatenate(tuple([a_tuple[i].t for i in range(n)]))
-    rtn.ts = np.concatenate(tuple([a_tuple[i].ts for i in range(n)]))
+    """Concatenate all tuple points into an AEData object"""
+    rtn = AEData()
+    n_points = len(a_tuple)
+    rtn.x_data = np.concatenate(tuple([a_tuple[i].x_data for i in range(n_points)]))
+    rtn.y_data = np.concatenate(tuple([a_tuple[i].y_data for i in range(n_points)]))
+    rtn.t_data = np.concatenate(tuple([a_tuple[i].t_data for i in range(n_points)]))
+    rtn.timestamp = np.concatenate(tuple([a_tuple[i].timestamp for i in range(n_points)]))
     return rtn
-
-    # np.concatenate(a_tuple)
